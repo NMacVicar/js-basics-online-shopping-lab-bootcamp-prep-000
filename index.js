4f1286692afda1f6d9a6ec66d9a59be7f450b78b
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 02:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don\'t have a credit card on file for you.")
   } else {
     var charge = total()
-    console.log(`Your total cost is ${charge}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is $${charge}, which will be charged to the card ${cardNumber}.`)
     cart = []
   }
 }
