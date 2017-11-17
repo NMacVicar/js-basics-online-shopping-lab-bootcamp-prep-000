91e182878477012e4c0575d566c2daadb5180992
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 01:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ function viewCart() {
 
 function total() {
   // write your code here
+  var total = 0
+  for (var i = 0; i < cart.length; i++){
+    for (item in cart[i]){
+      total += cart[i][item]
+    }
+  }
 }
 
 function removeFromCart(item) {
