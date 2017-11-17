910b98b0a23f84c7b26b983f9fb2c56e17a95cd2
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 02:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === ""){
+  if (cardNumber === null){
     console.log("Sorry, we don\'t have a credit card on file for you.")
   } else {
     var charge = total()
