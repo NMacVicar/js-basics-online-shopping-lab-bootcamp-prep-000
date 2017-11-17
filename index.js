91c5ab83085c655a2b4bf6b9b5d5959ba3063bf9
Neil MacVicar <n.macvicar9@gmail.com>
2017-11-17 01:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,10 +64,12 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnPropert(item)){
-      cart
-    }  
+      cart = [...cart.splice(0, i), ...cart.splice(i + 1)]
+      return cart
+    }
   }
  console.log("That item is not in your cart.")
+ return cart
 }
   // write your code here
 
